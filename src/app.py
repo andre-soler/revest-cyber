e97f5335f1742f727c550661ffd779096544c95d
André <andrec.soleresr@gmail.com>
2025-09-20 09:58:27 -0300

    fix: corrige SQL Injection no app.py

--- src/app.py
+++ src/app.py
@@ -3,6 +3,6 @@ import sqlite3
 def get_user(username):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
-    # ?? C¢digo vulner vel: SQL Injection
-    cursor.execute(f"SELECT * FROM users WHERE username = '{username}'")
+    # âœ… Seguro: consulta parametrizada
+    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
     return cursor.fetchall()
